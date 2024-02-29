5b54464aadda6a07e6fedba3e4b6055406725514
Faby-Baby <fabianbertrand94@gmail.com>
2024-02-29 18:48:40 -0400

    Db Connection

---
+++ billing/src/main/java/com/softwarequality/MysqlCon.java
@@ -0,0 +1,21 @@
+package com.softwarequality;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class MysqlCon {
+    public static void main(String[] args) {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://164.92.87.80:3306/software_quality", "softwarequality", "SoftwareQuality_375");
+            Statement stmt = con.createStatement();
+
+            ResultSet rs = stmt.executeQuery(null);
+
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+    }
+}
