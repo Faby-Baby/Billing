71bd3177ee7e00b3cb45f74dced4c7bb16c87dde
Faby-Baby <fabianbertrand94@gmail.com>
2024-02-29 18:01:04 -0400

    Files

---
+++ billing/src/main/java/com/softwarequality/App.java
@@ -0,0 +1,13 @@
+package com.softwarequality;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
